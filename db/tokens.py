7faea5ff3445f8627d4122c420ce2aeff53cff76
Daniel Oravec <danoravec@gmail.com>
2023-07-01 12:44:59 +0200

    Add jwt validation
    * Existence of the user is not checked,
      as we consider the expiration time enough
      for now.

--- db/tokens.py
+++ db/tokens.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from config import settings
 
 
@@ -9,3 +9,13 @@ def create_access_token(data: dict):
         "exp": datetime.utcnow() + timedelta(minutes=settings.jwt_expire_minutes)
     })
     return jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
+
+
+# If we wanted to be able to revoke access by deleting the user, then checking if the user exists would be needed here.
+# Since the token expires after a short time, we skip this check.
+def is_auth_token_valid(token: str) -> bool:
+    try:
+        payload = jwt.decode(token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm])
+        return payload.get("user_id") is not None
+    except JWTError:
+        return False
